88672317b42ede0cd3fd75c95f4bf6dd5b63602a
Nguyen Dang Toan <nguyendangtritoan2305@gmail.com>
2021-04-02 15:17:32 +0300

    Fix CORS

--- src/main/java/com/example/iwwof/controllers/AdminController.java
+++ src/main/java/com/example/iwwof/controllers/AdminController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/admin")
-@CrossOrigin("*")
+@CrossOrigin(origins = "http://localhost:3000")
 public class AdminController {
 
     private final AdminService adminService;
